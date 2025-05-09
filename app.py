39a34bf1318a2a5a541b3ed2545cab80921e6f38
GordonHolding <office@gordonholding.de>
2025-05-10 00:31:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import chainlit as cl
 @cl.on_message
 async def main(message):
     await cl.Message(content=f"Deine Nachricht war: {message.content}").send()
+
+if __name__ == "__main__":
+    cl.run(port=10000, host="0.0.0.0")
