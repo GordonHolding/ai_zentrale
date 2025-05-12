c67da890876ff44fc129d21e36f511e0a0a8e774
GordonHolding <office@gordonholding.de>
2025-05-12 22:47:56 +0200

    Create google_utils.py

---
+++ modules/google_utils.py
@@ -0,0 +1,6 @@
+from google.oauth2 import service_account
+
+def get_credentials():
+    return service_account.Credentials.from_service_account_file(
+        "/etc/secrets/service_account.json"
+    )
