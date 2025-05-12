d5731666afd2bf00cc0870cc74f0d514f04ad290
GordonHolding <office@gordonholding.de>
2025-05-13 01:07:21 +0200

    Create generate_token.py

---
+++ modules/generate_token.py
@@ -0,0 +1,21 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+
+# Scopes je nach Bedarf anpassen
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send"
+]
+
+def main():
+    creds_path = "0.0 SYSTEM/0.1 Zugangsdaten/client_secret_*.json"  # Pfad zur JSON-Datei anpassen!
+    
+    flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+    creds = flow.run_local_server(port=8000)
+
+    # Speichere Token lokal ab
+    with open("token.json", "w") as token:
+        token.write(creds.to_json())
+
+if __name__ == "__main__":
+    main()
