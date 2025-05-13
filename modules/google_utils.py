65087df37f325fb0be3ad04212b9cbed25e3814e
GordonHolding <office@gordonholding.de>
2025-05-13 21:41:56 +0200

    Update google_utils.py

--- modules/google_utils.py
+++ modules/google_utils.py
@@ -2,6 +2,6 @@ from google.oauth2 import service_account
 
 def get_credentials(scopes):
     return service_account.Credentials.from_service_account_file(
-        "/etc/secrets/service_account.json",
+        "/etc/secrets/service_account_office_at_gordonholding.json",
         scopes=scopes
     )
