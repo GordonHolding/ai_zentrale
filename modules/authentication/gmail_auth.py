caf7d4a4ab037941e8df01de62f8c1ba258d2466
GordonHolding <office@gordonholding.de>
2025-05-14 02:38:07 +0200

    Update gmail_auth.py

--- modules/authentication/gmail_auth.py
+++ modules/authentication/gmail_auth.py
@@ -1,15 +1,35 @@
 import os
 import json
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
-def get_gmail_credentials():
-    with open("/etc/secrets/client_secret_office_at_gordonholding.json", "r") as f:
-        creds_data = json.load(f)
+# Alle Scopes – einheitlich systemweit
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://www.googleapis.com/auth/gmail.insert",
+    "https://www.googleapis.com/auth/gmail.compose",
+    "https://www.googleapis.com/auth/gmail.metadata"
+]
 
-    flow = InstalledAppFlow.from_client_config(
-        creds_data,
-        scopes=["https://www.googleapis.com/auth/gmail.readonly"]
-    )
+def get_gmail_credentials(account_name="office_gordonholding"):
+    base_path = "/etc/secrets"
 
-    creds = flow.run_local_server(port=8000)
+    token_path = os.path.join(base_path, f"token_{account_name}.json")
+    client_path = os.path.join(base_path, f"client_secret_{account_name}.json")
+
+    creds = None
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    else:
+        with open(client_path, "r") as f:
+            creds_data = json.load(f)
+        flow = InstalledAppFlow.from_client_config(creds_data, scopes=SCOPES)
+        creds = flow.run_local_server(port=8000)
+        with open(token_path, "w") as token_file:
+            token_file.write(creds.to_json())
+
     return creds
