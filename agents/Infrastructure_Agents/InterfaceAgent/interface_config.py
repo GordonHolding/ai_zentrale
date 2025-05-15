d33ac06a48f581597685ffe2fe974b07eb22efdc
GordonHolding <office@gordonholding.de>
2025-05-15 17:13:04 +0200

    Create interface_config.py

---
+++ agents/Infrastructure_Agents/InterfaceAgent/interface_config.py
@@ -0,0 +1,13 @@
+# interface_config.py
+
+AGENT_LABELS = {
+    "mail": "MailAgent",
+    "memory": "MemoryAgent"
+}
+
+AGENT_ACTIONS = {
+    "mail": "process_emails",
+    "memory": "search_memory"
+}
+
+DEFAULT_MAIL_ACCOUNT = "office"
