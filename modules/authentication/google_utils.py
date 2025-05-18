920291e26dde57fb2a823bc02be5ecb4e3210399
GordonHolding <office@gordonholding.de>
2025-05-18 03:28:31 +0200

    Update google_utils.py

--- modules/authentication/google_utils.py
+++ modules/authentication/google_utils.py
@@ -1,8 +1,11 @@
+# google_utils.py
+
 from google.oauth2 import service_account
 import os
 
 def get_service_account_credentials(account_name="office_gordonholding", scopes=[]):
-    base_path = "/etc/secrets"
+    # Erlaube .env-Override für Pfad
+    base_path = os.getenv("GOOGLE_SECRET_PATH", "/etc/secrets")
     key_path = os.path.join(base_path, f"service_account_{account_name}.json")
 
     return service_account.Credentials.from_service_account_file(
