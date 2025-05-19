e441def455f4962710578a0d5511e823643fdfea
GordonHolding <office@gordonholding.de>
2025-05-19 18:27:15 +0200

    Delete modules/reasoning_intelligenz/sheet_reader.py

--- modules/reasoning_intelligenz/sheet_reader.py
+++
@@ -1,9 +0,0 @@
-from googleapiclient.discovery import build
-from modules.google_utils import get_credentials
-
-def read_sheet(spreadsheet_id, range_name):
-    creds = get_credentials(["https://www.googleapis.com/auth/spreadsheets.readonly"])
-    service = build("sheets", "v4", credentials=creds)
-    sheet = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id, range=range_name).execute()
-    return sheet.get("values", [])
