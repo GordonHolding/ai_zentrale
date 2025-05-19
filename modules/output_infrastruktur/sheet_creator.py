2e4d9f608ae9f69b2809c2036b563563bf03cb07
GordonHolding <office@gordonholding.de>
2025-05-19 18:27:45 +0200

    Delete modules/output_infrastruktur/sheet_creator.py

--- modules/output_infrastruktur/sheet_creator.py
+++
@@ -1,8 +0,0 @@
-from googleapiclient.discovery import build
-from modules.google_utils import get_credentials
-
-def create_sheet(title: str):
-    creds = get_credentials(["https://www.googleapis.com/auth/spreadsheets"])
-    service = build("sheets", "v4", credentials=creds)
-    sheet = service.spreadsheets().create(body={"properties": {"title": title}}).execute()
-    return sheet["spreadsheetId"]
